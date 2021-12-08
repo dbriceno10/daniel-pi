04f567a8cf99867c3bbf9e98a23d3d676baa5e59
Daniel Briceño <daniel.b100726@gmail.com>
2021-12-07 23:27:12 -0400

    revisión simple a backend

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -38,7 +38,7 @@ router.get('/', async (req, res, next) => {
       const dataAPI = await axios.get(
         'https://pokeapi.co/api/v2/pokemon?limit=40'
       ); //obtenemos pokemons del 1 al 40
-      // const pk = [...dataAPI.data.results]; // array de resultados que contienen la info para acceder a cada pokemon
+      const pk = [...dataAPI.data.results]; // array de resultados que contienen la info para acceder a cada pokemon
       const data = await Promise.all(
         pk.map((pokemon) => axios.get(pokemon.url)) //genero un array de promesas
       ); //paso mi array de promesas para resolverlo

84c98804e7a82bd2014a8dade355959d269e93f5
Daniel Briceño <daniel.b100726@gmail.com>
2022-06-27 09:03:12 -0400

    cambio para trabajar con DB remota

--- api/index.js
+++ api/index.js
@@ -23,6 +23,8 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    process.env.NODE_ENV === 'production'
+      ? console.log('the server is running')
+      : console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });

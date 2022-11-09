82b5ae45503b7ece9a833b0aa9247ce61943863f
Daniel Briceno <daniel.b100726@gmail.com>
2022-11-08 21:43:34 -0400

    set force in false

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     process.env.NODE_ENV === 'production'
       ? console.log('the server is running')

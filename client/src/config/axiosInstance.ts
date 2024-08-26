f5251737c2b6d3557922b629776fa36323c517d6
Daniel Briceno <daniel.b100726@gmail.com>
2024-08-26 00:05:40 -0400

    exteds timeout axios

--- client/src/config/axiosInstance.ts
+++ client/src/config/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosIntance = axios.create({
   baseURL: process.env.REACT_APP_API ?? "http://localhost:3001",
-  timeout: 30000,
+  timeout: 120000,
 });
 
 export default axiosIntance;
